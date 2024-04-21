0568afe15a479029068deb6b8c5a7315ef417d0f
SkimJazz <jb.skimjazz@outlook.com>
2024-04-21 16:23:15 +1000

    Installed ReactJS and TailwindCSS and create first template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function App() {
+    return (
+        <h1 className="text-2xl text-green-600 font-bold">
+            Good to go! Vite + React is synced up!
+        </h1>
+    )
+}
+
